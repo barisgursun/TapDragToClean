a8109801a9f039dc0acbd6cac847402a2d80163b
Baris Gursun <baris.gursun@hotmail.com>
2021-06-05 02:29:09 +0300

    mouse takip scripti eklendi

---
+++ Assets/follow_mouse.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow_mouse : MonoBehaviour
+{
+    Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        transform.position = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5));
+
+        
+
+        
+    }
+}
